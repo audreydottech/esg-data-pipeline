1ae90856ddc49511b1eedd7a6bb3b789f2211994
Audrey <ale314@uw.edu>
2024-11-26 08:01:00 -0600

    test

--- data_pipeline.py
+++ data_pipeline.py
@@ -2,12 +2,14 @@ from functions import *
 import time
 import datetime
 
+tickers = ['BXBLY', 'VWDRY', 'SMTGY']
+
 print("Starting data pipeline at ", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 print("----------------------------------------------")
 
 # Step 1: extract stock prices for the day
 t0 = time.time()
-getStockNews()
+getStockPrices(ticker_list=tickers)
 t1 = time.time()
 print("Step 1: Done")
 print("---> Stock prices downloaded in", str(t1-t0), "seconds", "\n")
@@ -15,7 +17,7 @@ print("---> Stock prices downloaded in", str(t1-t0), "seconds", "\n")
 
 # Step 2: extract stock news for the day
 t0 = time.time()
-getStockNews()
+getStockNews(ticker_list=tickers)
 t1 = time.time()
 print("Step 1: Done")
 print("---> Stock news downloaded in", str(t1-t0), "seconds", "\n")
