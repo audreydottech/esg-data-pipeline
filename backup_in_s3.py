70fe85878f09736c1d4753815b9c0c6975f6e5e8
Audrey <ale314@uw.edu>
2024-11-30 23:04:20 -0600

    add pipeline to s3

--- backup_in_s3.py
+++ backup_in_s3.py
@@ -1,15 +1,15 @@
 import datetime
+import os
+
 import polars as pl
 import s3fs
-
 import boto3
 
-# TO DO: Need to add secret to Github and schedule a job to write a new file to S3 once a month
-# TO DO: Configure IAM
 
-s3 = boto3.client('s3', aws_access_key_id='AKIA37K7GYJPJIHAE4W2',
-                  aws_secret_access_key='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')
+my_aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+my_aws_access_secret_key = os.getenv('AWS_ACCESS_SECRET_KEY')
 
+fs = s3fs.S3FileSystem(key=my_aws_access_key_id, secret=my_aws_access_secret_key)
 
 def grab_latest_news_from_Github():
     """
@@ -20,8 +20,6 @@ def grab_latest_news_from_Github():
     df = pl.read_json('data/esg-stock-news.json')
     df = df.with_columns(pl.col('providerPublishTime').str.to_date('%Y-%m-%d'))
     filtered_df = df.filter(pl.col("providerPublishTime").is_between(start_date, end_date))
-
-    fs = s3fs.S3FileSystem()
 
     destination = "s3://environmental-stock-data-bucket/esg-stock-news_" + str(
         datetime.datetime.now().strftime('%Y_%m_%d')) + '.json'
@@ -40,8 +38,6 @@ def grab_latest_prices_from_Github():
     df = df.with_columns(pl.col('timestamp').str.to_date('%Y-%m-%d %H:%M:%S'))
     filtered_df = df.filter(pl.col("timestamp").is_between(start_date, end_date))
 
-    fs = s3fs.S3FileSystem()
-
     destination = "s3://environmental-stock-data-bucket/esg-stock-prices_" + str(
         datetime.datetime.now().strftime('%Y_%m_%d')) + '.json'
 
@@ -53,10 +49,9 @@ def upload_file_to_s3():
     """
     Function to manually upload whole files
     """
+    s3 = boto3.client('s3', aws_access_key_id=my_aws_access_key_id,
+                      aws_secret_access_key=my_aws_access_secret_key)
     s3.upload_file('data/esg-stock-news.json', 'environmental-stock-data-bucket', 'esg-stock-news.json')
 
     s3.upload_file('data/esg-stock-prices.csv', 'environmental-stock-data-bucket', 'esg-stock-prices.csv')
 
-
-# TO DO: Snowflake job to grab the latest files from S3
-# TO DO: Can dbt just grab the files from S3 and transform them before loading them into Snowflake?
